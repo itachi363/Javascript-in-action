53e00a6ea26ea1d4d58f94a13837c58fbf277305
itachi363 <itachi363@yahoo.com>
2022-03-25 15:25:54 -0400

    finished for loop

--- worksheet.js
+++ worksheet.js
@@ -33,17 +33,30 @@
 
 // console.log(meal);
 
-let randomNumber = Math.floor(Math.random() * 11);
+// let randomNumber = Math.floor(Math.random() * 11);
 
-if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
-    console.log("Beatles")
-}
-else if(randomNumber === 3 || randomNumber === 4 || randomNumber === 5) {
-    console.log("Stones")
-}
-else if(randomNumber === 6 || randomNumber === 7 || randomNumber === 8) {
-    console.log("Floyd")
-}
-else if(randomNumber === 9 || randomNumber === 10) {
-    console.log("Hendrix")
+// if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
+//     console.log("Beatles")
+// }
+// else if(randomNumber === 3 || randomNumber === 4 || randomNumber === 5) {
+//     console.log("Stones")
+// }
+// else if(randomNumber === 6 || randomNumber === 7 || randomNumber === 8) {
+//     console.log("Floyd")
+// }
+// else if(randomNumber === 9 || randomNumber === 10) {
+//     console.log("Hendrix")
+// }
+
+// for(let i = 0; i < 7; i++) {
+//     console.log("JavaScript is cool!")
+// }
+
+// for(let i = 0; i < 11; i++) {
+//     console.log(i)
+// }
+
+for(let i = 0; i < 5; i++) {
+    console.log("hello")
+    console.log("goodbye")
 }
