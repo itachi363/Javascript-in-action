3bc975918614f551100f749cc5d254f14c05e1af
itachi363 <itachi363@yahoo.com>
2022-03-25 14:42:02 -0400

    did problems one and two

---
+++ worksheet.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let dayOfWeek = "Monday";
+console.log(dayOfWeek);
+dayOfWeek = "Friday";
+if(dayOfWeek === "Friday") {
+    console.log(dayOfWeek);
+}
+
+let animalInput = prompt("what is your favorite animal?")
+
+let colorInput = prompt("What is your favorite color?")
+
+console.log(`I've never seen a ${colorInput} ${animalInput}`)
