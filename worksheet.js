8f5a6c223aad15b50c01705d425724ce367555bf
itachi363 <itachi363@yahoo.com>
2022-03-25 15:13:11 -0400

    did meal time and random number generator

--- worksheet.js
+++ worksheet.js
@@ -1,14 +1,49 @@
 "use strict";
 
-let dayOfWeek = "Monday";
-console.log(dayOfWeek);
-dayOfWeek = "Friday";
-if(dayOfWeek === "Friday") {
-    console.log(dayOfWeek);
+// let dayOfWeek = "Monday";
+// console.log(dayOfWeek);
+// dayOfWeek = "Friday";
+// if(dayOfWeek === "Friday") {
+//     console.log(dayOfWeek);
+// }
+
+// let animalInput = prompt("what is your favorite animal?")
+
+// let colorInput = prompt("What is your favorite color?")
+
+// console.log(`I've never seen a ${colorInput} ${animalInput}`)
+
+// breakfast burrito, burrito, pizza
+
+// let timeOfDay = "tea";
+
+// let meal = "";
+
+// if(timeOfDay < 1200) {
+//     meal = "breakfast burrito";
+// }
+
+// else if(timeOfDay > 1200 && timeOfDay < 1700) {
+//     meal = "burrito";
+// }
+
+// else if(timeOfDay > 1700) {
+//     meal = "pizza"
+// }
+
+// console.log(meal);
+
+let randomNumber = Math.floor(Math.random() * 11);
+
+if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
+    console.log("Beatles")
 }
-
-let animalInput = prompt("what is your favorite animal?")
-
-let colorInput = prompt("What is your favorite color?")
-
-console.log(`I've never seen a ${colorInput} ${animalInput}`)
+else if(randomNumber === 3 || randomNumber === 4 || randomNumber === 5) {
+    console.log("Stones")
+}
+else if(randomNumber === 6 || randomNumber === 7 || randomNumber === 8) {
+    console.log("Floyd")
+}
+else if(randomNumber === 9 || randomNumber === 10) {
+    console.log("Hendrix")
+}
